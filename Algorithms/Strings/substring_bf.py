fa99ee67744f8a223ac6aab3c8dece9ed5d5dd5f
sjay05 <anusha.perumal@gmail.com>
2020-01-05 12:53:50 -0500

    brute force substring search

---
+++ Algorithms/Strings/substring_bf.py
@@ -0,0 +1,23 @@
+# Substring Search using Brute Force Approach
+"""
+Ex. Query:
+Text: abacbaabcabccabcaabb
+Pattern: abcaa
+"""
+
+def search(txt, pat):
+    a = len(pat)
+    b = len(txt)
+    i = 0
+    while (i <= (b - a) + 1):
+        x = 0
+        while (x < a):
+            if txt[i+x] != pat[x]:
+                break
+            x += 1
+        if (x == a):
+            print "Index = %s" %i
+        i += 1
+
+if __name__ == "__main__":
+    search("abacbaabcabccabcaabb", "abcaa")
